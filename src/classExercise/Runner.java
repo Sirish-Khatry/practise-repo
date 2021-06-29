d3ebd43906f273648266d3b33b11db3282a42be3
Sirish-Khatry <85874645+Sirish-Khatry@users.noreply.github.com>
2021-06-29 16:13:04 +0100

    Update Runner.java

--- src/classExercise/Runner.java
+++ src/classExercise/Runner.java
@@ -10,23 +10,23 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-//		Person staff1 = new Person();
-//		Person staff2 = new Person("Sirish", 23, "QA Consultant");
-//		Person staff3 = new Person("John", 30, "QA Team-Lead");
-//		Person staff4 = new Person("Joe", 25, "QA Lecturer");
-//		
-//		staff1.printDetails();
-//		staff2.printDetails();
-//		
-//		ArrayList<Person> staffList = new ArrayList<Person>();
-//		
-//		staffList.add(staff2);
-//		staffList.add(staff3);
-//		staffList.add(staff4);
-//	
-//		for(Person p : staffList) {
-//			p.printDetails();
-//		}
+		Person staff1 = new Person();
+		Person staff2 = new Person("Sirish", 23, "QA Consultant");
+		Person staff3 = new Person("John", 30, "QA Team-Lead");
+		Person staff4 = new Person("Joe", 25, "QA Lecturer");
+		
+		staff1.printDetails();
+		staff2.printDetails();
+		
+		ArrayList<Person> staffList = new ArrayList<Person>();
+		
+		staffList.add(staff2);
+		staffList.add(staff3);
+		staffList.add(staff4);
+	
+		for(Person p : staffList) {
+			p.printDetails();
+		}
 		
 		User u = new User();
 		u.greet();
